57c214af21721042583ec52f043ad4a65a7a742c
alexmakarevich <40922230+alexmakarevich@users.noreply.github.com>
2025-10-04 23:35:30 +0200

    move initial setup out of main

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ use crate::{
         org::delete_own_org,
         user::{create_user, delete_user_by_id, list_users},
     },
-    utils::api::AppState,
+    utils::{api::AppState, initial_setup::initial_setup},
 };
 
 #[derive(sqlx::FromRow)]
@@ -71,62 +71,7 @@ async fn main() {
 
     // check if app has already been initialized
     if !persistent_config.is_initialized {
-        let initial_org_title = dotenv::var("INITIAL_ORG").expect("INITIAL_ORG is not configured");
-        let initial_user_name =
-            dotenv::var("INITIAL_USER").expect("INITIAL_USERNAME is not configured");
-        let initial_user_password =
-            dotenv::var("INITIAL_PASSWORD").expect("INITIAL_PASSWORD is not configured");
-
-        let Ok(mut tx) = pool.begin().await else {
-            panic!("could not start init transaction")
-        };
-
-        let Ok(created_org) = sqlx::query!(
-            r#"INSERT INTO orgs (title) VALUES ($1) RETURNING id"#,
-            initial_org_title,
-        )
-        .fetch_one(&mut *tx)
-        .await
-        else {
-            panic!("could not create initial org")
-        };
-
-        let Ok(created_user) = sqlx::query!(
-            r#"INSERT INTO users (username, password, org_id) VALUES ($1, $2, $3) RETURNING id"#,
-            initial_user_name,
-            initial_user_password,
-            created_org.id,
-        )
-        .fetch_one(&mut *tx)
-        .await
-        else {
-            panic!("could not create initial user")
-        };
-
-        // role assginment
-        let _ = sqlx::query!(
-            r#"INSERT INTO role_assignments (user_id, role) VALUES ($1, 'super_admin')"#,
-            created_user.id,
-        )
-        .execute(&mut *tx)
-        .await
-        .map_err(|err| {
-            error!("{}", err);
-            panic!("could not create initial role assignment")
-        });
-
-        let _ = sqlx::query!("UPDATE config SET is_initialized=TRUE",)
-            .execute(&mut *tx)
-            .await
-            .map_err(|err| {
-                error!("{}", err);
-                panic!("could set initial config")
-            });
-
-        let _ = tx.commit().await.map_err(|err| {
-            error!("{}", err);
-            panic!("failed to initialize application")
-        });
+        initial_setup(&pool).await
     }
 
     let state = AppState { pg_pool: pool };
