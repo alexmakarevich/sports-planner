bc66c1564d4ec6994666e972668052e0047b8ecd
alexmakarevich <40922230+alexmakarevich@users.noreply.github.com>
2025-10-06 19:21:50 +0200

    remove unused

--- src/entities/user.rs
+++ src/entities/user.rs
@@ -1,4 +1,4 @@
-use log::{debug, error};
+use log::debug;
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -15,18 +15,6 @@ use axum::{
     response::{IntoResponse, Response},
     Extension, Json,
 };
-
-// For sqlx
-// all the fields - not sure if needed, but will leave here for now
-#[derive(Debug, Deserialize, Serialize, sqlx::FromRow)]
-#[allow(non_snake_case)] // TODO: why though? it's all snaky anyway
-pub struct UserModel {
-    pub id: String,
-    pub username: String,
-    pub password: String,
-    pub created_at: Option<chrono::NaiveDateTime>,
-    pub updated_at: Option<chrono::NaiveDateTime>,
-}
 
 #[derive(Deserialize)]
 pub struct CreateUser {
